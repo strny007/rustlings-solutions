85b64d62a64a4f79bd4cb5c32ca5523c1e0204b3
Pavel Strnad <strny007@gmail.com>
2022-10-10 08:11:27 +0200

    solutions

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -10,10 +10,16 @@
 // Write a function that calculates the price of an order of apples given
 // the quantity bought. No hints this time!
 
-// I AM NOT DONE
+
 
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples(apples: u32) -> u32 {
+    if apples < 41 {
+        apples * 2
+    } else {
+        apples
+    }
+}
 
 // Don't modify this function!
 #[test]
--- exercises/quiz2.rs
+++ exercises/quiz2.rs
@@ -18,7 +18,6 @@
 // - The output element is going to be a Vector of strings.
 // Execute `rustlings hint quiz2` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
 
 pub enum Command {
     Uppercase,
@@ -30,11 +29,19 @@ mod my_module {
     use super::Command;
 
     // TODO: Complete the function signature!
-    pub fn transformer(input: ???) -> ??? {
+    pub fn transformer(input: Vec<(String, Command)>) -> Vec<String> {
         // TODO: Complete the output declaration!
-        let mut output: ??? = vec![];
+        let mut output: Vec<String> = vec![];
         for (string, command) in input.iter() {
-            // TODO: Complete the function body. You can do it!
+            match command {
+                Command::Uppercase => output.push(string.to_uppercase().to_string()),
+                Command::Trim => output.push(string.trim().to_string()),
+                Command::Append(i) => {
+                    let mut s = string.clone();
+                    s.push_str(&(0..*i).map(|_| "bar").collect::<String>());
+                    output.push(s)
+                }
+            }
         }
         output
     }
@@ -42,8 +49,7 @@ mod my_module {
 
 #[cfg(test)]
 mod tests {
-    // TODO: What do we have to import to have `transformer` in scope?
-    use ???;
+    use my_module::transformer;
     use super::Command;
 
     #[test]
